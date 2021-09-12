ce236765dc4bb087b6d189795a8ceace415da178
Carson Holloway <carsonalh@gmail.com>
2021-09-12 11:21:02 +1000

    Created landing page.

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,7 +1,32 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
+import './Home.css';
+import hero from './hero.png';
+import logo from './logo.png';
 
 function Home(props) {
-    return 'betcoin home';
+    return (
+        <div className="Home">
+            <div className="header">
+                <div className="title">
+                    <h1>Betcoin</h1>
+                    <p>
+                        Cryptocurrency, Gambling<br />
+                        Good separate<br />
+                        Better together
+                    </p>
+                </div>
+                <img src={logo} alt="Betcoin logo" />
+            </div>
+            <img src={hero} alt="Hero" />
+            <div className="sign-up">
+                <Link to="/signup">
+                    <button>Sign Up Today</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
