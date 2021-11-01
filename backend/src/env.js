900092c17877ff264011e356b042c139d60a5ae8
Carson Holloway <carsonalh@gmail.com>
2021-11-01 13:50:09 +1000

    Add database port environment variable

--- backend/src/env.js
+++ backend/src/env.js
@@ -1,6 +1,8 @@
 module.exports = {
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_USER: process.env.DB_USER,
+  DB_PORT:
+    (process.env.DB_PORT && Number.parseInt(process.env.DB_PORT)) || 3306,
   DB_PASSWORD: process.env.DB_PASSWORD,
   DB_DATABASE_NAME: process.env.DB_DATABASE_NAME,
 };
