001c21b4fda0bad0c9eb9a8fa3362f17c7324161
Carson Holloway <carsonalh@gmail.com>
2021-11-02 14:49:54 +1000

    Add schema validation for request/response
    Use yup to add schema validation for requests/responses of each route.

--- backend/src/api.js
+++ backend/src/api.js
@@ -1,6 +1,8 @@
 const app = require("express").Router();
 const ethers = require("ethers");
 const createHttpError = require("http-errors");
+
+const Schema = require("./schema");
 const { Controller } = require("./controller");
 
 const { HttpError } = createHttpError;
@@ -14,65 +16,16 @@ const catchHttpError = (req, res, next) => (err) => {
   }
 };
 
-/*
-    Used to create a new user and get user data. If the user already exists,
-    then this route should just get that data and return what is necessary back
-    to the frontend.
-
-    POST /users
-
-    // Request
-    {
-        user: {
-            email: string,
-            // If the user doesn't exist, this is required
-            name?: string,
-            password: string
-        }
-    }
-
-    // Response
-    {
-        user: {
-            id: string, // This will be base64 of the email
-            email: string,
-            name: string,
-            publicKey: string, // Hex digest
-            privateKey: string, // Hex digest
-        }
-    }
-*/
-
 app.post("/users", (req, res, next) => {
-  Controller.postUser(req.body.user)
+  const body = Schema.UserRequest.cast(req.body);
+
+  Controller.postUser(body.user)
     .then((user) => {
-      res.status(200).json({ user });
+      const response = Schema.UserResponse.cast({ user });
+      res.status(200).json(response);
     })
     .catch(catchHttpError(req, res, next));
 });
-
-/*
-    Used to create and accept friend request. Cannot be denied (only pending).
-
-    POST /users/:userId/friends
-
-    // Request
-    {
-        friend: {
-            email: string
-        }
-    }
-
-    // Response
-    {
-        friend: {
-            email: string,
-            name: string,
-            pending: boolean,
-	    publicKey: string
-        }
-    }
-*/
 
 app.post("/users/:userId/friends", (req, res, next) => {
   // TODO: Abstract this validation/casting
@@ -80,42 +33,27 @@ app.post("/users/:userId/friends", (req, res, next) => {
     ethers.utils.base64.decode(req.params.userId)
   );
 
-  // TODO: Validate the body; for now an exception right here is the best move
-  Controller.postFriend(email, req.body.friend.email)
+  const body = Schema.FriendRequest.cast(req.body);
+
+  Controller.postFriend(email, body.friend.email)
     .then((friend) => {
-      res.status(200).json({ friend });
+      const response = Schema.FriendResponse.cast({ friend });
+      res.status(200).json(response);
     })
     .catch(catchHttpError(req, res, next));
 });
 
-/*
-    No query data is required here, as :userId is all that is needed to get
-    their list of friends.
-
-    GET /users/:userId/friends
-
-    // Request
-    {
-    }
-
-    // Response
-    {
-        friends: [{
-            email: string,
-            name: string,
-            pending: boolean,
-	    publicKey: string
-        }]
-    }
- */
 app.get("/users/:userId/friends", (req, res, next) => {
+  // TODO: Find a way to verify this
+
   const userEmail = ethers.utils.toUtf8String(
     ethers.utils.base64.decode(req.params.userId)
   );
 
   Controller.getFriends(userEmail)
     .then((friends) => {
-      res.status(200).json({ friends });
+      const response = Schema.FriendsResponse.cast({ friends });
+      res.status(200).json(response);
     })
     .catch(catchHttpError(req, res, next));
 });
---
+++ backend/src/schema.js
@@ -0,0 +1,67 @@
+const yup = require("yup");
+
+const Schema = {};
+
+/**
+ * A "friend" entity
+ */
+Schema.Friend = yup.object().shape({
+  email: yup.string().required(),
+  name: yup.string().required(),
+  pending: yup.boolean().required(),
+  publicKey: yup.string().required(),
+});
+
+/**
+ * A "user" entity
+ */
+Schema.User = yup.object().shape({
+  id: yup.string().required(),
+  email: yup.string().required(),
+  name: yup.string().required(),
+  publicKey: yup.string().required(),
+  privateKey: yup.string().length(64).required(),
+});
+
+/**
+ * Schema for the request to the `POST /users` route.
+ */
+Schema.UserRequest = yup.object().shape({
+  user: yup.object().shape({
+    email: yup.string().max(320).required(),
+    password: yup.string().required(),
+    name: yup.string(),
+  }),
+});
+
+/**
+ * Schema for the response to the `POST /users` route.
+ */
+Schema.UserResponse = yup.object().shape({
+  user: Schema.User,
+});
+
+/**
+ * Schema for the request to the `POST /users/:id/friends`.
+ */
+Schema.FriendRequest = yup.object().shape({
+  friend: yup.object().shape({
+    email: yup.string().required(),
+  }),
+});
+
+/**
+ * Schema for the response to the `POST /users/:id/friends`.
+ */
+Schema.FriendResponse = yup.object().shape({
+  friend: Schema.Friend,
+});
+
+/**
+ * Schema for the response to the `GET /users/:id/friends`.
+ */
+Schema.FriendsResponse = yup.object().shape({
+  friends: yup.array().of(Schema.Friend),
+});
+
+module.exports = Schema;
